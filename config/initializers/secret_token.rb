0c3770442a90dcf976519e33930165b8796fd444
Chad Baum <chadbaum@gmail.com>
2016-05-17 12:48:01 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Battler::Application.config.secret_key_base = '6bbe41bb3cdd761e91dc0af1ac139a31415daab2cd0985747aa8ce0871d0338e6dafde9fcf3706be4e0b7106a0756ba202c58546a334008dc9e5571b1ebfaf93'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Battler::Application.config.session_store :cookie_store, key: '_battler_session'
