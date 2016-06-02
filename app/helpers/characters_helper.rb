e730d3a9b261301739b6cbcc3a6fe025271d578c
Chad Baum <chadbaum@gmail.com>
2016-06-02 18:52:33 +0000

    Refactored character model

--- app/helpers/characters_helper.rb
+++ app/helpers/characters_helper.rb
@@ -1,12 +1,5 @@
 module CharactersHelper
 
-  
-
-  def get_active_level(character)
-    active_job = CharacterJob.find_by character_id: character, active: true
-    active_job.level
-  end
-
   def job_names
     job_names = []
     Job.all.each do |job|
--- app/models/character.rb
+++ app/models/character.rb
@@ -16,16 +16,18 @@ class Character < ActiveRecord::Base
   end
 
   def active_job_level
-    active_job.level
+    active_character_job.take.level
   end
 
   private
 
+  def active_character_job
+    character_jobs.where active: true
+  end
+
   def active_job
-    active_character_job = character_jobs.where active: true
     active_job_id = active_character_job.take.job_id
-    active_job = Job.find active_job_id
-    active_job
+    jobs.find active_job_id
   end
 
 end
