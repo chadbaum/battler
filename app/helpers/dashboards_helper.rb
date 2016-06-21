6304fdb024a3af27e6ed4134480a27aa5e2f8c8a
Chad Baum <chadbaum@gmail.com>
2016-06-21 16:47:32 +0000

    Added icons for inventory panel

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,31 +1,22 @@
 module DashboardsHelper
 
-  def head_slot
-    @character.head ? @character.head.item.name : "Empty"
-  end
-
-  def chest_slot
-    @character.chest ? @character.chest.item.name : "Empty"
-  end
-
-  def feet_slot
-    @character.feet ? @character.feet.item.name : "Empty"
-  end
-
-  def accessory1_slot
-    @character.accessories[0] ? @character.accessories[0].item.name : "Empty"
-  end
-
-  def accessory2_slot
-    @character.accessories[1] ? @character.accessories[1].item.name : "Empty"
-  end
-
-  def right_slot
-    @character.right ? @character.right.item.name : "Empty"
-  end
-
-  def left_slot
-    @character.left ? @character.left.item.name : "Empty"
+  def equipped(slot)
+    case slot
+    when 'head'
+      @character.head ? @character.head.item.name : "Empty"
+    when 'chest'
+      @character.chest ? @character.chest.item.name : "Empty"
+    when 'feet'
+      @character.feet ? @character.feet.item.name : "Empty"
+    when 'accessory1'
+      @character.accessories[0] ? @character.accessories[0].item.name : "Empty"
+    when 'accessory2'
+      @character.accessories[1] ? @character.accessories[1].item.name : "Empty"
+    when 'right'
+      @character.right ? @character.right.item.name : "Empty"
+    when 'left'
+      @character.left ? @character.left.item.name : "Empty"
+    end
   end
 
 end
