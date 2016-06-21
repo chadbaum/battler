dfdf6a4e2af3cceb3b3aa48fc0c6666d1c117ef8
Chad Baum <chadbaum@gmail.com>
2016-06-21 15:49:03 +0000

    Refactored empty checks for equipped items in dashboard view into dashboard helper methods

--- app/helpers/dashboards_helper.rb
+++ app/helpers/dashboards_helper.rb
@@ -1,2 +1,31 @@
 module DashboardsHelper
+
+  def head_slot
+    @character.head ? @character.head.item.name : "Empty"
+  end
+
+  def chest_slot
+    @character.chest ? @character.chest.item.name : "Empty"
+  end
+
+  def feet_slot
+    @character.feet ? @character.feet.item.name : "Empty"
+  end
+
+  def accessory1_slot
+    @character.accessories[0] ? @character.accessories[0].item.name : "Empty"
+  end
+
+  def accessory2_slot
+    @character.accessories[1] ? @character.accessories[1].item.name : "Empty"
+  end
+
+  def right_slot
+    @character.right ? @character.right.item.name : "Empty"
+  end
+
+  def left_slot
+    @character.left ? @character.left.item.name : "Empty"
+  end
+
 end
